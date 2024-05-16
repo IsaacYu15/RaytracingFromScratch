5be9ed73fe4a9a126c85eb7718bbaa8d14e19462
Isaac Yu <yuisaac@outlook.com>
2024-05-16 17:17:10 -0400

    initialize repo

---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by yuisa on 2024-05-16.
+//
+
+#ifndef RAYTRACINGFROMSCRATCH_MAIN_H
+#define RAYTRACINGFROMSCRATCH_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //RAYTRACINGFROMSCRATCH_MAIN_H
