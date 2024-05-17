f2143293b0b9f8ac16bcf20fd4b09236c01a9b3c
Isaac Yu <yuisaac@outlook.com>
2024-05-17 18:56:10 -0400

    starting vec3 class

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include<cstdlib>
 
 int main() {
 
-    // Image
-
+    // Specify image dimensions
     int image_width = 256;
     int image_height = 256;
 
-    // Render
-
+    // Render ppm image
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.0;
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            int ir = int(255 * r);
+            int ig = int(255 * g);
+            int ib = int(255 * b);
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
