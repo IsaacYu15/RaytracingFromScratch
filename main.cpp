2b5ac14264f9d1dd75b88d3e76d516ab2236836d
Isaac Yu <yuisaac@outlook.com>
2024-06-01 21:26:06 -0400

    sphere intersection

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3&center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot (r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r)
 {
+    if (hit_sphere(point3(0,0, -1), 0.3, r))
+    {
+        return color (1, 0 , 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
+    auto a = 0.7*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
