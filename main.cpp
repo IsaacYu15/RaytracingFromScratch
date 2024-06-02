8b2cb37fff8374c0734098669f975255e53e1001
Isaac Yu <yuisaac@outlook.com>
2024-06-02 15:46:02 -0400

    ray normals

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3&center, double radius, const ray& r)
+double hit_sphere(const point3&center, double radius, const ray& r)
 {
     vec3 oc = center - r.origin();
     auto a = dot (r.direction(), r.direction());
@@ -11,14 +11,23 @@ bool hit_sphere(const point3&center, double radius, const ray& r)
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
 
-    return (discriminant >= 0);
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant) ) / (2.0 * a); //returns location of intersection
+    }
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0,0, -1), 0.3, r))
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0)
     {
-        return color (1, 0 , 0);
+        vec3 N  = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
