f5bc48420d8f1864e57212d2e14bd5101c9dbef2
Isaac Yu <yuisaac@outlook.com>
2024-08-15 21:34:18 -0400

    glass material and depth of field

--- main.cpp
+++ main.cpp
@@ -25,8 +25,8 @@ int main() {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    for (int a = -3; a < 3; a++) {
-        for (int b = -3; b < 3; b++) {
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
@@ -66,8 +66,8 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 1200;
-    cam.samples_per_pixel = 500;
-    cam.max_depth         = 50;
+    cam.samples_per_pixel = 50;
+    cam.max_depth         = 20;
 
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
